42885b8b1a7db3c58f72b22c00344fd6d0138dda
legendof1st99-ops <legendof1st99@gmail.com>
2026-01-12 21:30:46 +0700

    Add files via upload

---
+++ Coffee Machine.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CoffeeMachine
+{
+    public class CoffeeMachine
+    {
+        int water;
+        int coffee;
+        int milk;
+        int chocolate;
+
+        // constructor
+        public CoffeeMachine(int water, int coffee, int milk, int chocolate)
+        {
+            this.water = water;
+            this.coffee = coffee;
+            this.milk = milk;
+            this.chocolate = chocolate;
+        }
+
+        void Refill()
+        {
+            Console.WriteLine("วัตถุดิบไม่พอ กำลังเติม...");
+            water += 500;
+            coffee += 50;
+            milk += 30;
+            chocolate += 30;
+        }
+
+        public void MakeBlackCoffee()
+        {
+            if (water < 300 || coffee < 20)
+                Refill();
+
+            water -= 300;
+            coffee -= 20;
+            Console.WriteLine("ชงกาแฟดำเสร็จแล้ว ☕");
+        }
+
+        public void MakeMocha()
+        {
+            if (water < 300 || coffee < 20 || chocolate < 10)
+                Refill();
+
+            water -= 300;
+            coffee -= 20;
+            chocolate -= 10;
+            Console.WriteLine("ชงมอคค่าเสร็จแล้ว ☕");
+        }
+
+        public void MakeLatte()
+        {
+            if (water < 300 || coffee < 20 || milk < 10)
+                Refill();
+
+            water -= 300;
+            coffee -= 20;
+            milk -= 10;
+            Console.WriteLine("ชงลาเต้เสร็จแล้ว ☕");
+        }
+
+        public void MakeChocolate()
+        {
+            if (water < 300 || chocolate < 20)
+                Refill();
+
+            water -= 300;
+            chocolate -= 20;
+            Console.WriteLine("ชงช็อกโกแลตเสร็จแล้ว ☕");
+        }
+
+        public void ShowStock()
+        {
+            Console.WriteLine("\nวัตถุดิบคงเหลือ:");
+            Console.WriteLine("น้ำ: " + water + " g");
+            Console.WriteLine("กาแฟ: " + coffee + " g");
+            Console.WriteLine("นม: " + milk + " g");
+            Console.WriteLine("ช็อกโกแลต: " + chocolate + " g");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CoffeeMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CoffeeMachine machine = new CoffeeMachine (1000, 100, 50, 50);
+            string input = "";
+
+            while (input != "0")
+            {
+                Console.WriteLine("\n---------------------------");
+                Console.WriteLine(" Coffee Machine ");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("1: กาแฟดำ");
+                Console.WriteLine("2: มอคค่า");
+                Console.WriteLine("3: ลาเต้");
+                Console.WriteLine("4: ช็อกโกแลต");
+                Console.WriteLine("5: แสดงวัตถุดิบ");
+                Console.WriteLine("0: ออกจากโปรแกรม");
+                Console.Write("เลือกเมนู: ");
+
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        machine.MakeBlackCoffee();
+                        break;
+                    case "2":
+                        machine.MakeMocha();
+                        break;
+                    case "3":
+                        machine.MakeLatte();
+                        break;
+                    case "4":
+                        machine.MakeChocolate();
+                        break;
+                    case "5":
+                        machine.ShowStock();
+                        break;
+                    case "0":
+                        Console.WriteLine("ออกจากโปรแกรม");
+                        break;
+                    default:
+                        Console.WriteLine("เลือกเมนูไม่ถูกต้อง");
+                        break;
+                }
+            }
+        }
+    }
+}
